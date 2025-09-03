d5f3d634b1964fdb06c93c195d9d9469726c8c55
Jatin <codrjatin@gmail.com>
2025-09-04 01:49:31 +0530

    changed bg-img

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const navbar = document.getElementById("navbar");
 const nav_a = document.getElementsByClassName("nav-a");
 
 window.addEventListener("scroll",()=>{
-    if(window.scrollY > (0.999)*window.innerHeight){
+    if(window.scrollY > (0.5)*window.innerHeight){
         navbar.classList.add("scrolled");
         nav_a.classList.add("scrolled");
     } else {
